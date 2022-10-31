c97220c97e0e7f12dc41129a0357b459ef67bbb8
baikdk <baikdk@g.hongik.ac.kr>
2022-10-31 16:25:42 +0900

    사라짐 적용 완
    ㄴㅇㅅ

--- New Unity Project/Assets/MiniGame1/JudgeCommand.cs
+++ New Unity Project/Assets/MiniGame1/JudgeCommand.cs
@@ -5,78 +5,60 @@ using UnityEngine.UI;
 
 public class JudgeCommand : MonoBehaviour
 {
-    [SerializeField] GameObject A,B,C,D,E,F;
-    static int i = 0;
+    [SerializeField] GameObject state;
+    public int i = 0;
 
     public void ButtonA()
     {
-        while(i <= 5)
+        if (state.transform.GetChild(i).name == "A(Clone)")
         {
-            if (A==gameObject.transform.Find("Canvas").Find("Prefab").GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 
     public void ButtonB()
     {
-        while (i <= 5)
+        if (state.transform.GetChild(i).name == "B(Clone)")
         {
-            if (B == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 
     public void ButtonC()
     {
-        while (i <= 5)
+        if (state.transform.GetChild(i).name == "C(Clone)")
         {
-            if (C == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 
     public void ButtonD()
     {
-        while (i <= 5)
+        if (state.transform.GetChild(i).name == "D(Clone)")
         {
-            if (D == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 
     public void ButtonE()
     {
-        while (i <= 5)
+        if (state.transform.GetChild(i).name == "E(Clone)")
         {
-            if (E == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 
     public void ButtonF()
     {
-        while (i <= 5)
+        if (state.transform.GetChild(i).name == "F(Clone)")
         {
-            if (F == gameObject.transform.GetChild(1).GetChild(2).GetChild(i))
-            {
-                Destroy(gameObject.transform.GetChild(1).GetChild(2).GetChild(i));
-                i++;
-            }
+            state.transform.GetChild(i).gameObject.SetActive(false);
+            ++i;
         }
     }
 }
