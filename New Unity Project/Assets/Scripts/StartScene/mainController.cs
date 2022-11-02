5fd6cd486525b87880678629f45ad0f7d7cf35b4
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2022-11-03 00:56:58 +0900

    Revert "레벨업 팝업 생성"
    This reverts commit b3e334b1665c84200a9fe5aef8755e64125fef6e.

--- New Unity Project/Assets/Scripts/StartScene/mainController.cs
+++ New Unity Project/Assets/Scripts/StartScene/mainController.cs
@@ -1,25 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class mainController : MonoBehaviour
 {
-    public void MainToMinigame()  // 화면 전환
+    public void MainToMinigame()
     {
         SceneManager.LoadScene("MiniGame2");
     }
 
-    public void MainToHome() // 화면 전환
+    public void MainToHome()
     {
         SceneManager.LoadScene("HomeScene");
-    }
-
-
-    public GameObject lvlpopup = null;
-    public void BandLevelUP() // 합주하기 버튼
-    {
-        lvlpopup.SetActive(true);
     }
 }
