b3e334b1665c84200a9fe5aef8755e64125fef6e
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2022-11-03 00:30:27 +0900

    레벨업 팝업 생성

--- New Unity Project/Assets/Scripts/StartScene/mainController.cs
+++ New Unity Project/Assets/Scripts/StartScene/mainController.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class mainController : MonoBehaviour
 {
-    public void MainToMinigame()
+    public void MainToMinigame()  // 화면 전환
     {
         SceneManager.LoadScene("MiniGame2");
     }
 
-    public void MainToHome()
+    public void MainToHome() // 화면 전환
     {
         SceneManager.LoadScene("HomeScene");
+    }
+
+
+    public GameObject lvlpopup = null;
+    public void BandLevelUP() // 합주하기 버튼
+    {
+        lvlpopup.SetActive(true);
     }
 }
