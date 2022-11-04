9c3893b2e8b3bfdd9897bc57afa2aa48380a7374
baikdk <baikdk@g.hongik.ac.kr>
2022-11-04 15:39:11 +0900

    Revert "SceneRandom, ClearPanel"
    This reverts commit ef06161995f72697cef83a1b58ba8bf808c6bf48.

--- New Unity Project/Assets/Scripts/StartScene/mainController.cs
+++ New Unity Project/Assets/Scripts/StartScene/mainController.cs
@@ -7,18 +7,7 @@ public class mainController : MonoBehaviour
 {
     public void MainToMinigame()
     {
-        int number = Random.Range(1, 3);
-
-        if (number == 1)
-        {
-            SceneManager.LoadScene("MiniGame1");
-        }
-
-        else if(number == 2)
-        {
-            SceneManager.LoadScene("MiniGame2");
-        }
-        
+        SceneManager.LoadScene("MiniGame2");
     }
 
     public void MainToHome()
