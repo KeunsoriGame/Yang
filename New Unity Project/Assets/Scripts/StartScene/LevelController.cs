460814191a95bb8d918c947083368de1258a2792
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2022-11-04 18:40:03 +0900

    레벨, 스탯 구현

--- New Unity Project/Assets/Scripts/StartScene/LevelController.cs
+++ New Unity Project/Assets/Scripts/StartScene/LevelController.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
+    public Text levelText;
+    int level = 1;
+
     int statSum = 0;
-    public Slider loading;
 
     public GameObject load;
     public GameObject levelup;
     public GameObject levelupfail;
+    public GameObject statSc;
+
+
+    
 
     void Start()
     {
-        
+        levelText.text = "밴드 Lv." + level.ToString();
     }
 
     void Update()
@@ -25,20 +31,51 @@ public class LevelController : MonoBehaviour
     public void LevelManager()  // 합주하기 버튼 클릭 시
     {
         load.SetActive(true);
-        Invoke("LevelUP", 3f);
+
+        statSum = statSc.GetComponent<Stat>().GetStatSum();
+
+        if (LevelCmp(statSum))
+        {
+            Invoke("LevelUP", 3f);
+            level++;
+            levelText.text = "밴드 Lv." + level.ToString();
+        }
+        else
+            Invoke("LevelUPFail", 3f);
+        
     }
 
-    void LevelUP()
+    void LevelUP()  // 합주 성공
     {
         load.SetActive(false);
         levelup.SetActive(true);
+
     }
 
-    void LevelUPFail()
+    void LevelUPFail()  // 합주 실패
     {
         load.SetActive(false);
+        levelupfail.SetActive(true);
     }
 
-    
+    bool LevelCmp(int sum) // 레벨 별 조건과 스탯 합 비교
+    {
+        int s = sum;
+        switch (level)
+        {
+            case 1:
+                return s >= 100;
+            case 2:
+                return s >= 220;
+            case 3:
+                return s >= 340;
+            case 4:
+                return s >= 460;
+            default:
+                return true;
+        }
+    }
+
+   
 
 }
